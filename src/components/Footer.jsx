b699dfda513e21ed7f1807ec6000e586cd6ac88a
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-01 13:38:13 +0530

    footer-routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,19 +14,32 @@ function Footer() {
           <h2 className="rights"> All Rights Reserved. Bold.org, Inc</h2>
         </div>
         <div className="footer-btns">
-          <button className="f-btn1">Create a Scholarship</button>
-          <button className="f-btn2">Apply to a Scholarship</button>
+          <Link to="/joindoner">
+            <button className="f-btn1">Create a Scholarship</button>
+          </Link>
+          <Link to="/joinappli">
+            <button className="f-btn2">Apply to a Scholarship</button>
+          </Link>
         </div>
       </div>
       <div className="down-footer">
         <div className="down1">
           <h1 className="doner">DONORS</h1>
-          <p>How it works</p>
-          <p>Doner Credits </p>
-          <p>Award Features</p>
-
-          <p>Pricing</p>
-          <p>Doner FAQ</p>
+          <Link to="/how-it-works">
+            <p>How it works</p>
+          </Link>
+          <Link to="/doner-credits">
+            <p>Doner Credits </p>
+          </Link>
+          <Link to="/award">
+            <p>Award Features</p>
+          </Link>
+          <Link to="/pricing">
+            <p>Pricing</p>
+          </Link>
+          <Link to="/faq">
+            <p>Doner FAQ</p>
+          </Link>
         </div>
         <div className="down2">
           <h1 className="doner">applicants</h1>
@@ -36,7 +49,6 @@ function Footer() {
 
           <p>Scholarships for Women</p>
           <p>Scholarships for Nursing Students</p>
-          <p>Scholarships for Black Students</p>
           <p>Scholarships for High School Seniors</p>
           <p>Ambassadors</p>
         </div>
