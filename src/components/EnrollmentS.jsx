7dd0763830c28c3f2426f2616db178de631bf83a
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-03 15:35:49 +0530

    pop-up done

--- src/components/EnrollmentS.jsx
+++ src/components/EnrollmentS.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import EnrollForm from "./EnrollForm";
 
 function EnrollmentS() {
-  return <div></div>;
+  return (
+    <div>
+      <EnrollForm />
+    </div>
+  );
 }
 
 export default EnrollmentS;
