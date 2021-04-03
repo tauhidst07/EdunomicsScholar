2cc804ba461408d20fb55d769eda074689235f08
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-03 10:20:01 +0530

    privacy

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Privacy() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default Privacy;
