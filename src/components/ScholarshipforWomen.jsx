113333692de374a642816c6891d264e2ca97a28c
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-05 12:59:31 +0530

    initiate woemn scholarship

--- src/components/ScholarshipforWomen.jsx
+++ src/components/ScholarshipforWomen.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import SchlorBox from "./SchlorBox";
 
 function ScholarshipforWomen() {
   return (
     <div>
       <Header />
+      <div className="lady-1">
+        <div className="b1">
+          <img className="im1" src={boy} alt="st" />
+          <div className="text1">
+            <h1 className="un-head">
+              22 Easy Scholarships with Upcoming Deadlines
+            </h1>
+            <p className="un-p">
+              The Top Easy, No-Essay Scholarships With Upcoming Deadlines
+            </p>
+          </div>
+        </div>
+      </div>
+      <SchlorBox />
+      <SchlorBox />
+
+      <SchlorBox />
+
+      <SchlorBox />
+
       <Footer />
     </div>
   );
