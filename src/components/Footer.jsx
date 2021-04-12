f084570ff6ca8c16accbf7a9159977c7a835c9d5
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-12 16:59:26 +0530

    changed to pride edunomics.in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
           <Link to="/" className="logo-main" style={{ cursor: "pointer" }}>
             <img className="log-pic" src={edu} alt="logo" />
           </Link>
-          <h2 className="rights"> All Rights Reserved. Edunomics</h2>
+          <h2 className="rights"> All Rights Reserved pride.edunomics.in</h2>
         </div>
         <div className="footer-btns">
           <Link to="/joindoner">
