e542d3425eb1fb8aaa79070aff4f33ce6b034a74
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-22 11:44:52 +0530

    fix loader in applicants

--- src/components/AllApliScholarship.jsx
+++ src/components/AllApliScholarship.jsx
@@ -7,9 +7,11 @@ import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
 import CakeIcon from "@material-ui/icons/Cake";
 import DashboardHeader from "./DashboardHeader";
 import Footer from "./Footer";
+import Loader from "react-loader-spinner";
 
 function AllApliScholarship() {
   const [data, setData] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchMyApi() {
@@ -18,6 +20,8 @@ function AllApliScholarship() {
       );
       response = await response.json();
       setData([response]);
+      setLoading(false);
+
       // console.log(data)
     }
 
@@ -26,90 +30,98 @@ function AllApliScholarship() {
   return (
     <div>
       <DashboardHeader isActive={true} />
-      <h1 style={{ marginTop: "2rem", textAlign: "center" }}>
-        scholarships {data === 0 ? 0 : data[0].count}
-      </h1>
-      {data === 0
-        ? console.log("not fetched")
-        : data[0].scholarships.map((e) => (
-            <div key={e._id} className="main-boxshadow ">
-              <div className="sc-box ">
-                <img src={boy} alt="st" />
-                <div className="sc-head">
-                  <h3>{e.name}</h3>
-                  <span>Funded by</span>
-                  <p>
-                    Become eligible for this scholarship by showcasing that you
-                    know how to navigate your student loan options.
-                  </p>
-                </div>
+      {loading ? (
+        <div style={{ textAlign: "center", alignItems: "center" }}>
+          <Loader type="ThreeDots" color="grey" height={100} width={100} />
+        </div>
+      ) : (
+        <div>
+          <h1 style={{ marginTop: "2rem", textAlign: "center" }}>
+            scholarships {data === 0 ? 0 : data[0].count}
+          </h1>
+          {data === 0
+            ? console.log("not fetched")
+            : data[0].scholarships.map((e) => (
+                <div key={e._id} className="main-boxshadow ">
+                  <div className="sc-box ">
+                    <img src={boy} alt="st" />
+                    <div className="sc-head">
+                      <h3>{e.name}</h3>
+                      <span>Funded by</span>
+                      <p>
+                        Become eligible for this scholarship by showcasing that
+                        you know how to navigate your student loan options.
+                      </p>
+                    </div>
 
-                <div className="apply">
-                  <Link
-                    className="apply-bt1"
-                    to={`/apli-view-scholar/${e._id}&${e.createdBy}`}
-                  >
-                    Apply to scholarship
-                  </Link>
-                  <button className="apply-bt2">Nominate a friend</button>
-                </div>
-              </div>
+                    <div className="apply">
+                      <Link
+                        className="apply-bt1"
+                        to={`/apli-view-scholar/${e._id}&${e.createdBy}`}
+                      >
+                        Apply to scholarship
+                      </Link>
+                      <button className="apply-bt2">Nominate a friend</button>
+                    </div>
+                  </div>
 
-              <div className="elgi-field sc-box">
-                <div className="field-el">
-                  <h2>
-                    <LocalConvenienceStoreIcon />
-                    Education level
-                  </h2>
-                  <p>Any</p>
+                  <div className="elgi-field sc-box">
+                    <div className="field-el">
+                      <h2>
+                        <LocalConvenienceStoreIcon />
+                        Education level
+                      </h2>
+                      <p>Any</p>
+                    </div>
+                    <div className="field-el">
+                      <h2>
+                        <AttachMoneyIcon />
+                        Amount
+                      </h2>
+                      <p>{e.awardAmount}</p>
+                    </div>
+                    <div className="field-el">
+                      <h2>
+                        <CardGiftcardIcon /> Scholarships awarded
+                      </h2>
+                      <p>{e.winnersLimit}</p>
+                    </div>
+                    <div className="field-el">
+                      <h2>
+                        <CakeIcon />
+                        Deadline
+                      </h2>
+                      <p>
+                        <span>
+                          {
+                            new Date(e.awardDate.toString())
+                              .toString()
+                              .split(" ")[1]
+                          }
+                          -
+                        </span>
+                        <span style={{ marginLeft: ".1rem" }}>
+                          {
+                            new Date(e.awardDate.toString())
+                              .toString()
+                              .split(" ")[2]
+                          }
+                        </span>
+                        -
+                        <span style={{ marginLeft: ".1rem" }}>
+                          {
+                            new Date(e.awardDate.toString())
+                              .toString()
+                              .split(" ")[3]
+                          }
+                        </span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="field-el">
-                  <h2>
-                    <AttachMoneyIcon />
-                    Amount
-                  </h2>
-                  <p>{e.awardAmount}</p>
-                </div>
-                <div className="field-el">
-                  <h2>
-                    <CardGiftcardIcon /> Scholarships awarded
-                  </h2>
-                  <p>{e.winnersLimit}</p>
-                </div>
-                <div className="field-el">
-                  <h2>
-                    <CakeIcon />
-                    Deadline
-                  </h2>
-                  <p>
-                    <span>
-                      {
-                        new Date(e.awardDate.toString())
-                          .toString()
-                          .split(" ")[1]
-                      }
-                      -
-                    </span>
-                    <span style={{ marginLeft: ".1rem" }}>
-                      {
-                        new Date(e.awardDate.toString())
-                          .toString()
-                          .split(" ")[2]
-                      }
-                    </span>
-                    -
-                    <span style={{ marginLeft: ".1rem" }}>
-                      {
-                        new Date(e.awardDate.toString())
-                          .toString()
-                          .split(" ")[3]
-                      }
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+        </div>
+      )}
 
       <Footer />
     </div>
