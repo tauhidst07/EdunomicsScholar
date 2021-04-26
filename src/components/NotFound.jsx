ac54e60a67dd93adc292765801a04803b211cc93
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-26 10:09:23 +0530

    change not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../styles/notfound.css";
+import not from "../media/not.gif";
 
 function NotFound() {
-  return <div class="wrapper"></div>;
+  return (
+    <div class="wrapper">
+      <img src={not} alt="" />
+    </div>
+  );
 }
 
 export default NotFound;
