8bf484f2c094ebd830df03e52d5969c8035c09ab
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-27 16:14:41 +0530

    fixing folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../src/styles/App.css";
 import LandingPage from "./components/LandingPage";
-import SignUp from "./components/SignUp";
+import SignUp from "./components/donar/SignUp";
 import SignUpApplicant from "./components/SignUpApplicant";
 import DonerGrantPage from "./components/DonerGrantPage";
-import Login from "./components/Login";
+import Login from "./components/donar/Login";
 import { Route, Switch } from "react-router-dom";
 import AppliGrantPage from "./components/AppliGrantPage";
 import JoinDoner from "./components/JoinDoner";
