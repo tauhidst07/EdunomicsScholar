5efc5dbf13092c8b8894be0bb17030246939e230
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-04-27 17:28:24 +0530

    Revert "about done"
    This reverts commit 75c057e270c19a02342727fab63b0ce9cf00ea69.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "../../styles/about.css";
-import Header from "../Header";
-import Footer from "../Footer";
+import "../styles/about.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function About() {
   return (
