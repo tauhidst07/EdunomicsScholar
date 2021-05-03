fa5cb0239d0273075164cf719e1236997de1d87c
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-05-03 11:01:10 +0530

    initiate reducers store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import DataProvider from "./components/redux/store";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-
+  <DataProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </DataProvider>,
   document.getElementById("root")
 );
