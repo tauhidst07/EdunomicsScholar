f77c103995e05711b8e4e2496d3bdd3f85cf1734
Rakeshbaghel021 <rkrkumar021@gmail.com>
2021-05-10 10:58:56 +0530

    initaite

---
+++ src/components/redux/reducers/authAppliReducer.js
@@ -0,0 +1,14 @@
+import { TYPES } from "../actions/authAction";
+
+const initialState = {};
+
+const authAppliReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TYPES.GET_PROFILE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default profileReducer;
